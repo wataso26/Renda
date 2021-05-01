c108298cff4e424542e2edcf00d243e620e8b7db
lifeistech <2wata2wata2@gmail.com>
2021-05-01 13:02:36 +0900

    連打ゲームの完成

---
+++ app/src/main/java/app/wataso_/watanabe/renda/MainActivity.kt
@@ -0,0 +1,22 @@
+package app.wataso_.watanabe.renda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var tapCount =0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tapButton.setOnClickListener{
+
+            tapCount = tapCount +1
+
+            countText.text =tapCount.toString()
+
+        }
+    }
+}
